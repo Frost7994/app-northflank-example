0ce174e144bdf0042b255354eb2f20e339e917c2
tommy byrne <tommybyrne9@gmail.com>
2023-03-31 14:33:39 +0100

    added prisma, integrated with next-auth and deployed db

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,12 @@
+// utils
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/lib/prisma";
 
 export const authOptions = {
+    // add your own database connection here
+    adapter: PrismaAdapter(prisma),
     // Configure one or more authentication providers
     providers: [
         GoogleProvider({

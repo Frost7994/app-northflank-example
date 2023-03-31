93017c5555efcf6884f84f74341a64e541c5de54
tommy byrne <tommybyrne9@gmail.com>
2023-03-31 14:03:00 +0100

    added next-auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
+// utils
+import { SessionProvider } from "next-auth/react";
+
 // styling
 import "@/styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+export default function App({ Component, pageProps: { session, ...pageProps } }) {
+    return (
+        <SessionProvider session={session}>
+            <Component {...pageProps} />
+        </SessionProvider>
+    );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,40 @@
+// utils
+import { useSession, signIn, signOut } from "next-auth/react";
+
 const Home = () => {
-    return <div>Home</div>;
+    // sesion destructure
+    const { data: session } = useSession();
+
+    // display if user is logged in
+    if (session) {
+        return (
+            <div className="w-screen h-screen flex justify-center items-center">
+                <div className="flex items-center flex-col gap-2">
+                    <h3 className="text-2xl font-medium">Logged In</h3>
+                    <p className="text-lg">Welcome {session.user.email}</p>
+                    <button
+                        className="border rounded-md px-3 py-2 font-medium transition-all border-rose-500 text-rose-500 hover:bg-rose-500/20"
+                        onClick={() => signOut()}>
+                        Sign Out
+                    </button>
+                </div>
+            </div>
+        );
+    }
+
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <div className="flex flex-col  items-center gap-2">
+                <h3 className="text-2xl font-medium">Not Logged In</h3>
+                <p className="text-lg">Sign in to see your details</p>
+                <button
+                    className="border rounded-md px-3 py-2 font-medium transition-all border-sky-500 text-sky-500 hover:bg-sky-500/20"
+                    onClick={() => signIn("google")}>
+                    Sign In
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Home;

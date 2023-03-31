403fe466c98647e6fe74b1ab6ef425bb85b39a44
tommy byrne <tommybyrne9@gmail.com>
2023-03-31 14:41:46 +0100

    updated next-auth to use github for issue with deployed redirect with google

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
 // utils
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import GithubProvider from "next-auth/providers/github";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@/lib/prisma";
 
@@ -9,9 +9,9 @@ export const authOptions = {
     adapter: PrismaAdapter(prisma),
     // Configure one or more authentication providers
     providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET,
+        GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
         }),
         // ...add more providers here
     ],
